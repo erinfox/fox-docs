c8b83e9e79372bfaca8acadc7802f5503bf69884
erinfox <erinkayfox@gmail.com>
2025-02-20 10:39:04 -0800

    add random_symbol

--- app/controllers/random_symbols_controller.rb
+++ app/controllers/random_symbols_controller.rb
@@ -1,6 +1,12 @@
 class RandomSymbolsController < ApplicationController
   def index
-    ruby_symbols = RubySymbol.all
-    @random_item = ruby_symbols.order('RANDOM()').limit(1)
+    # ruby_symbols = RubySymbol.all
+    # @random_item = ruby_symbols.order('RANDOM()').limit(1).first
+
+    @random_item = RubySymbol.all.sample
+
   end
 end
+
+
+
