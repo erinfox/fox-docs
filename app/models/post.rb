660bd08857c2c13022eaaafbd57e479ef5f601c1
erinfox <erinkayfox@gmail.com>
2025-02-28 11:03:07 -0800

    add formatted_publish_date, publish-title, and self.available_cat

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  def formatted_publish_date
+    created_at.strftime('%b %d, %Y')
+  end
+
+  def publish_title
+    'Published on: ' + formatted_publish_date
+  end
+
+  def self.available_cat
+    ['reading', 'pets']
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'formatted_publish_date' do
+    it 'formats the date' do
+     post = Post.create(title: 'Reading 101', content: 'words')
+
+     expect(post.formatted_publish_date).to eq('Feb 28, 2025')
+    end
+  end
+  describe 'publish_title' do
+    it 'publish_title' do
+      post = Post.create(title: 'Reading 101', content: 'words')
+
+      expect(post.publish_title).to eq('Published on: Feb 28, 2025')
+    end
+  end
+
+  describe 'self.available_cat' do
+    it 'self.available_cat' do
+      # Post.available_cat
+      expect(Post.available_cat).to eq(['reading', 'pets'])
+    end
+  end
 end
